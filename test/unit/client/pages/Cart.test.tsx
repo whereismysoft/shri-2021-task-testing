9f7fb6510b1ed7fd9706e8b6c6d6fa4c18af2e59
George <kotovyuriy27@gmail.com>
2021-10-23 23:26:31 +0300

    upd Cart test

--- test/unit/client/pages/Cart.test.tsx
+++ test/unit/client/pages/Cart.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import '@testing-library/react/dont-cleanup-after-each'
-import { render } from "@testing-library/react";
+// import '@testing-library/react/dont-cleanup-after-each'
+import { render, screen } from "@testing-library/react";
 import { fireEvent } from "@testing-library/dom";
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 
 import { Cart } from '../../../../src/client/pages/Cart';
+import { DefaultStoreType } from '../../helpers/store'
 
 const cartItems = {
     '1': {
@@ -23,55 +24,59 @@ const cartItems = {
 
 type CartItemsType = keyof typeof cartItems
 
-interface DefaultState { details: {}, lastOrdered?: undefined }
+const defaultState = { details: {}, cart: cartItems, latestOrderId: 1 };
 
-const defaultState = { details: {}, cart: cartItems };
-
-function createRootReducer(initialState: DefaultState) {
+function createRootReducer(initialState: DefaultStoreType) {
     return (state = initialState, action: { type: string }) => {
         switch (action.type) {
             case 'CLEAR_CART':
-                return { ...state, cart: {}, latestOrderId: undefined } as DefaultState
+                return { ...state, cart: {}, latestOrderId: undefined } as DefaultStoreType
             default:
                 return state
         }
     }
 }
 
-const App = ({ initStore }: { initStore: DefaultState }) => (
-    <BrowserRouter>
-        <Provider store={createStore(createRootReducer(initStore))}>
-            <Cart />
-        </Provider>
-    </BrowserRouter>
-)
+function renderApp(appStore: DefaultStoreType) {
+    const App = ({ initStore }: { initStore: DefaultStoreType }) => (
+        <BrowserRouter>
+            <Provider store={createStore(createRootReducer(initStore))}>
+                <Cart />
+            </Provider>
+        </BrowserRouter>
+    )
 
-const { container, findByText, getByRole, debug, rerender } = render(<App initStore={defaultState} />)
-let itemNode: undefined | HTMLElement
+    return render(<App initStore={appStore} />)
+}
 
 it('Cart should contain checkout form', () => {
+    const { container } = renderApp(defaultState)
     const formBlock = container.querySelector('.Form')
 
     expect(formBlock).toBeInTheDocument()
 })
 
 it('Cart should have added item', async () => {
+    const { findByText } = renderApp(defaultState)
     const blockWithItemName = await findByText(cartItems['1'].name)
-    itemNode = blockWithItemName.parentElement
 
     expect(blockWithItemName).toBeDefined()
 });
 
-it('Added item have correct price', () => {
-    const childrens = Array.from(itemNode.children)
+it('Added item have correct price', async () => {
+    const { findByText } = renderApp(defaultState)
+    const cartItemRowNameCell = await findByText(cartItems['1'].name)
+    const childrens = Array.from(cartItemRowNameCell.parentElement.children)
     const priceBlock = childrens.find(el => el.className.includes('Price'))
     const expectedPrice = cartItems['1'].price.toString()
 
     expect(priceBlock.textContent).toMatch(expectedPrice)
 })
 
-it('Added item have correct total price', () => {
-    const childrens = Array.from(itemNode.children)
+it('Added item have correct total price', async () => {
+    const { findByText } = renderApp(defaultState)
+    const cartItemRowNameCell = await findByText(cartItems['1'].name)
+    const childrens = Array.from(cartItemRowNameCell.parentElement.children)
     const totalPriceBlock = childrens.find(el => el.className.includes('Total'))
     const expectedTotalPrice = (cartItems['1'].price * cartItems['1'].count).toString()
 
@@ -79,6 +84,7 @@ it('Added item have correct total price', () => {
 })
 
 it('Cart should have correct order price', async () => {
+    const { findByText } = renderApp(defaultState)
     const totalPriceTitleBlock = await findByText('Order price:')
     const cartKeys = Object.keys(cartItems) as Array<CartItemsType>
     const expectedTotalPrice = cartKeys.reduce((sum, key: CartItemsType) => sum + cartItems[key].count * cartItems[key].price, 0)
@@ -86,17 +92,33 @@ it('Cart should have correct order price', async () => {
     expect(totalPriceTitleBlock.nextSibling.textContent).toMatch(expectedTotalPrice.toString())
 })
 
+it('Last order information is invisible in cart', () => {
+    const { queryByText } = renderApp({ details: {}, cart: cartItems, latestOrderId: 1 })
+
+    expect(queryByText(/order # has been successfully completed\./i)).toBeNull()
+})
+
 it('On clear button click cart is empty', async () => {
+    const { getByRole, findByText } = renderApp(defaultState)
     const clearButton = getByRole('button', { name: /clear shopping cart/i })
 
     fireEvent.click(clearButton)
 
     const element = await findByText(/Cart is empty. Please select products/)
+
     expect(element).toBeDefined()
 })
 
 it('Empty cart should not contain checkout form', () => {
+    const { container } = renderApp({ details: {}, cart: {} })
     const formBlock = container.querySelector('.Form')
 
     expect(formBlock).toBeFalsy()
+})
+
+it('Last order information is visible in empty cart', () => {
+    const { getByText } = renderApp({ details: {}, cart: {}, latestOrderId: 1 })
+    const lastOrderInfoBlock = getByText(/order # has been successfully completed\./i)
+
+    expect(lastOrderInfoBlock).toBeInTheDocument()
 })
